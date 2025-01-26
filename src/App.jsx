307322a75b03d5e2e8a874373664172ae2da7cd4
CPunch <sethtstubbs@gmail.com>
2025-01-25 20:27:13 -0600

    more better dropdown :3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import MapboxExample from "./map.jsx"; 
+import MapboxExample from "./map.jsx";
+import Dropdown from "./Dropdown.jsx";
 
 const App = () => {
   return (
     <div>
       <div className="text-bold text-center text-3xl">hiiii :D</div>
       <div style={{height: "500px"}}>
+        <Dropdown />
         <MapboxExample />
       </div>
     </div>
