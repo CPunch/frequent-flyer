79d49590715fa8bde3b263de9ca9772b0e1d4193
Kaitlin Martin <kxm1944@mavs.uta.edu>
2025-01-26 10:49:28 -0600

    making koi happy

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -71,7 +71,6 @@ const Dropdown = ({ className }) => {
 
   return (
     <Listbox value={selected} onChange={setSelected}>
-      <Label className="block text-sm/6 font-medium text-gray-900">Assigned to</Label>
       <div className="relative mt-2">
         <ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
           <span className="col-start-1 row-start-1 flex items-center gap-3 pr-6">
@@ -87,7 +86,7 @@ const Dropdown = ({ className }) => {
         
         <ListboxOptions
           transition
-          className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base ring-1 shadow-lg ring-black/5 focus:outline-hidden data-leave:transition data-leave:duration-100 data-leave:ease-in data-closed:data-leave:opacity-0 sm:text-sm"
+          className="absolute z-20 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base ring-1 shadow-lg ring-black/5 focus:outline-hidden data-leave:transition data-leave:duration-100 data-leave:ease-in data-closed:data-leave:opacity-0 sm:text-sm"
         >
           {people.map((person) => (
             <ListboxOption
