95cfaf1bda42e4857a484934766f507f7d353a4b
uta-kdt2020 <123854840+uta-kdt2020@users.noreply.github.com>
2025-01-25 22:20:39 -0600

    Readded Dropbox, Added Logo
    Oops I deleted it earlier my bad. Here have a Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import MapboxExample from "./map.jsx"; 
+import Dropdown from "./Dropdown.jsx";
 
 const App = () => {
   return (
     <div>
+      <div>
+        <img src="src\assets\logo.png" class="justify-self-center w-100 md:w-160 lg:w-200" />
+      </div>
       <div className="text-bold text-center text-3xl">Frequent Flyer</div>
+      <div class="justify-self-center w-100 md:w-160 lg:w-200">
+        <Dropdown />
+      </div>
       <div style={{height: "500px"}} class="justify-self-center w-100 md:w-160 lg:w-200 border-5 border-solid border-gray-500 rounded-md">
         <MapboxExample />
       </div>
