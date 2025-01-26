5d095143df637fdde41a06c0169b742b7040c070
uta-kdt2020 <123854840+uta-kdt2020@users.noreply.github.com>
2025-01-25 21:52:14 -0600

    Map Style Change
    Added border styling, center justified, implemented responsive sizing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import MapboxExample from "./map.jsx";
-import Dropdown from "./Dropdown.jsx";
+import React from "react";
+import MapboxExample from "./map.jsx"; 
 
 const App = () => {
   return (
     <div>
-      <div className="text-bold text-center text-3xl">hiiii :D</div>
-      <div style={{height: "500px"}}>
-        <Dropdown />
+      <div className="text-bold text-center text-3xl">Frequent Flyer</div>
+      <div style={{height: "500px"}} class="justify-self-center w-100 md:w-160 lg:w-200 border-5 border-solid border-gray-500 rounded-md">
         <MapboxExample />
       </div>
     </div>
