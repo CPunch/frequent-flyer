54687d828f2d1a39f5920e1cc2222f2d6f98ce03
CPunch <sethtstubbs@gmail.com>
2025-01-26 08:58:40 -0600

    added airports

--- service/db.js
+++ service/db.js
@@ -27,19 +27,43 @@ const routesSchema = new mongoose.Schema({
     destinationAirportID: {type: String, required: true,},
     codeshare: {type: String, required: false,},
     stops: {type: String, required: false,},
-    equipment: {type: String, required: false,},
+    equipment: {type: String, required: false,}
 });
 const routesTbl = mongoose.model('routes', routesSchema);
 
+// schema for airports
+const airportSchema = new mongoose.Schema({
+  id: { type: String, required: false },
+  ident: { type: String, required: false },
+  type: { type: String, required: false },
+  name: { type: String, required: false },
+  latitude_deg: { type: Number, required: false },
+  longitude_deg: { type: Number, required: false },
+  elevation_ft: { type: String, required: false },
+  continent: { type: String, required: false },
+  iso_country: { type: String, required: false },
+  iso_region: { type: String, required: false },
+  municipality: { type: String, required: false },
+  scheduled_service: { type: String, required: false },
+  gps_code: { type: String, required: false },
+  iata_code: { type: String, required: false },
+  local_code: { type: String, required: false },
+  home_link: { type: String, required: false },
+  wikipedia_link: { type: String, required: false },
+  keywords: { type: String, required: false }
+});
+const airportsTbl = mongoose.model('airports', airportSchema);
+
 const importCSVData = () => {
-  const results = []; // Array to hold parsed data
+  const routes = []; // Array to hold parsed data
+  const airports = []
 
   // Read CSV file and parse it
   fs.createReadStream('./routes.csv')
     .pipe(csv())
     .on('data', (data) => {
       // Push each row into the results array
-      results.push({
+      routes.push({
           airline: data.airline,
           airlineID: parseInt(data.airlineID),
           sourceAirport: data.sourceAirport,
@@ -56,8 +80,43 @@ const importCSVData = () => {
       await routesTbl.deleteMany({})
 
       // Insert parsed data into MongoDB
-      await routesTbl.insertMany(results)
-      console.log('Data imported successfully!');
+      await routesTbl.insertMany(routes)
+      console.log('routes imported successfully!');
+    });
+
+  // Read CSV file and parse it
+  fs.createReadStream('./airports.csv')
+    .pipe(csv())
+    .on('data', (data) => {
+      // Push each row into the results array
+      airports.push({
+        id: parseInt(data.id),
+        ident: data.ident,
+        type: data.type,
+        name: data.name,
+        latitude_deg: parseFloat(data.latitude_deg),
+        longitude_deg: parseFloat(data.longitude_deg),
+        elevation_ft: data.elevation_ft,
+        continent: data.continent,
+        iso_country: data.iso_country,
+        iso_region: data.iso_region,
+        municipality: data.municipality,
+        scheduled_service: data.scheduled_service,
+        gps_code: data.gps_code,
+        iata_code: data.iata_code,
+        local_code: data.local_code,
+        home_link: data.home_link,
+        wikipedia_link: data.wikipedia_link,
+        keywords: data.keywords
+      });
+    })
+    .on('end', async () => {
+      // clear table
+      await airportsTbl.deleteMany({})
+
+      // Insert parsed data into MongoDB
+      await airportsTbl.insertMany(routes)
+      console.log('airport imported successfully!');
     });
 };
 
